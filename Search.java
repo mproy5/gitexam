df1157eb560e6a8717c83244db22725dc8e05dc4
Mahendra Pratap Roy <mahi.mpr@gmail.com>
2023-02-02 15:02:31 +0530

    Linear Search added

---
+++ Search.java
@@ -0,0 +1,21 @@
+public class Search
+{
+    
+    public static void main(String[] args)
+    {
+         int[] arr1 = {7, 9, 11, 12};
+         
+        int key = 11;
+        // Linear Search
+       for(int i = 0; i < arr.length; i++)
+       {
+            if(arr1[i] == key)
+            {
+                System.our.println("found at index at " + i);
+            }
+
+       }
+       System.our.println("not found");
+
+    }
+}
